e9d3af0f68eabe164b1b36482a1f69c7e1583a6a
Javan Poirier <me@javanpoirier.com>
2019-07-06 18:04:42 -0300

    updated skills

--- src/containers/Skills/Skills.js
+++ src/containers/Skills/Skills.js
@@ -57,13 +57,26 @@ export default class Skills extends Component {
     render() {
         return (
             <Block minHeight={'100vh'} style={{display: "flex", alignItems: "center"}}>
-                <h1>Skills</h1>
-                <Text size="1.25em" responsive="1.75vmax">Various skill sets in a wide range of different programs and technologies. This is probably not the best way to show my experience. At least they look cool!</Text>
+                <h1>Experience</h1>
+                <Text size="1.25em" responsive="1.75vmax">A representation of my experience with various sectors and technologies, not my capabilities. That would be outrageous!</Text>
                 <br />
                 <Container>
                     <Wrapper>
                         <Chart>
-                            <Pie percent={60} scale="0.9" />
+                            <Pie percent={86} scale="0.9" />
+                        </Chart>
+                        <Text margin="auto" style={{fontSize: "1.15em"}}>Web/Mobile</Text>
+                        <List>
+                            <ListItem>React.js</ListItem>
+                            <ListItem>Express.js</ListItem>
+                            <ListItem>React Native</ListItem>
+                            <ListItem>Redux</ListItem>
+                            <ListItem>Native Android/iOS</ListItem>
+                        </List>
+                    </Wrapper>
+                    <Wrapper>
+                        <Chart>
+                            <Pie percent={67} scale="0.9" />
                         </Chart>
                         <Text margin="auto" style={{fontSize: "1.15em"}}>Multimedia</Text>
                         <List>
@@ -75,7 +88,7 @@ export default class Skills extends Component {
                     </Wrapper>
                     <Wrapper>
                         <Chart>
-                            <Pie percent={70} scale="0.9" />
+                            <Pie percent={43} scale="0.9" />
                         </Chart>
                         <Text margin="auto" style={{fontSize: "1.15em"}}>Electronics/Robotics</Text>
                         <List>
@@ -84,27 +97,15 @@ export default class Skills extends Component {
                             <ListItem>Arduino</ListItem>
                         </List>
                     </Wrapper>
-                    <Wrapper>
-                        <Chart>
-                            <Pie percent={40} scale="0.9" />
-                        </Chart>
-                        <Text margin="auto" style={{fontSize: "1.15em"}}>Servers/Networking</Text>
-                        <List>
-                            <ListItem>CentOS</ListItem>
-                            <ListItem>Nginx</ListItem>
-                            <ListItem>SSH/FTP</ListItem>
-                        </List>
-                    </Wrapper>
                     <Wrapper>                        
                         <Chart>
-                            <Pie percent={80} scale="0.9" />
+                            <Pie percent={72} scale="0.9" />
                         </Chart>
                         {this.props.scrollTrigger}
                         <Text margin="auto"style={{fontSize: "1.15em"}}>3D Printing/Modeling</Text>
                         <List>
                             <ListItem>RepRap</ListItem>
                             <ListItem>Makerbot</ListItem>
-
                             <ListItem>SketchUp</ListItem>
                             <ListItem>Simplify3D</ListItem>
                         </List>
